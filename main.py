177b294bb8cde69b3ad1e8f63c39cbcb669d922f
Isaax <Isaax@DESKTOP-PUB147T>
2025-03-20 19:46:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ class MyPlugin(Star):
         super().__init__(context)
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''helloworld''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"你发送了{message_str}!") # 发送一条纯文本消息
+    #@filter.command("helloworld")
+    #async def helloworld(self, event: AstrMessageEvent):
+    #    '''helloworld''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    #    user_name = event.get_sender_name()
+    #    message_str = event.message_str # 用户发的纯文本消息字符串
+    #    message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+    #    logger.info(message_chain)
+    #    yield event.plain_result(f"你发送了{message_str}!") # 发送一条纯文本消息
 
     @event_message_type(EventMessageType.ALL)
     async def on_all_message(self, event: AstrMessageEvent):
         '''来首'''
         message_str = event.message_str # 获取消息的纯文本内容
         if message_str.startswith("来首"):
-            message_musicname=message_str[3:]
+            message_musicname=message_str[2:]
             yield event.plain_result(f"未找到歌曲{message_musicname}")
         else:
-            yield event.plain_result(f"{message_musicname}")
+            yield event.plain_result(f"{message_str}")
 
     async def terminate(self):
         '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
