5d2ddfc75eec543540c4817c3bdb66ce220c9958
Isaax <Isaax@DESKTOP-PUB147T>
2025-03-20 19:48:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ class MyPlugin(Star):
         if message_str.startswith("来首"):
             message_musicname=message_str[2:]
             yield event.plain_result(f"未找到歌曲{message_musicname}")
-        else:
-            yield event.plain_result(f"{message_str}")
+        #else:
+        #    yield event.plain_result(f"{message_str}")
 
     async def terminate(self):
         '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
