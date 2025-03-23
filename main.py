0ba1a2da18695b5bbeb1e6e7afef10d219c0abb9
Isaax <Isaax@DESKTOP-PUB147T>
2025-03-23 21:42:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,15 +74,14 @@ class MyPlugin(Star):
             if len(chance)<2:
                 yield event.plain_result("选项太少！")
             else:
-                result=event.make_result()
-                result.chain=[At(qq=event.get_sender_id()),
-                Plain(f"  的 {args[1]} 选择建议如下：")]
+                result=f"{user_name} 的 {args[1]} 选择建议如下："
                 result_pair=sorted(chance.items(),key=lambda d:d[1], reverse=True)
                 for key, value in result_pair:
-                    result_chance=f"\r\n{key} ({value/summary*100:.2f}%)"
-                    result.chain.append(Plain(result_chance))
-                result.use_t2i(False)
-                yield result # 发送一条纯文本消息
+                    result_chance=f"\n{key} ({value/summary*100:.2f}%)"
+                    result+=result_chance
+    #    message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+    #    logger.info(message_chain)
+                yield event.plain_result(result) # 发送一条纯文本消息
 
     @event_message_type(EventMessageType.ALL)
     async def on_all_message(self, event: AstrMessageEvent):
